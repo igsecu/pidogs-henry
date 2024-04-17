b44f5bde68e3e3368f99ce8b3a01fe9d3d7324c1
Ignacio Cunial <igsecu@hotmail.com>
2024-04-17 17:56:24 -0300

    Added GET / route

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const dogController = require("../controllers/dogs");
+
+// Get all dogs
+router.get("/", dogController.getDogs);
+
 module.exports = router;
