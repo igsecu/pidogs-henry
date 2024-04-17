b038dfa974deb8539e3d0d3b73ed64dfd49e9b47
Ignacio Cunial <igsecu@hotmail.com>
2024-04-17 17:55:49 -0300

    Imported dog router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const dogsRouter = require("/dogs");
+const dogsRouter = require("./dogs");
 
 // Specify router root route
 router.use("/dogs", dogsRouter);
