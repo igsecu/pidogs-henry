c62b48fac01059af6113b94af490dc35f16ba0ef
Ignacio Cunial <igsecu@hotmail.com>
2024-04-17 19:14:05 -0300

    Created getDogsByTemperament service

--- api/src/services/dogs.js
+++ api/src/services/dogs.js
@@ -1,6 +1,8 @@
 const Dog = require("../models/Dog");
 const Temperament = require("../models/Temperament");
 
+const { Op } = require("sequelize");
+
 // Get dogs
 const getDogs = async (page) => {
   const results = [];
@@ -95,7 +97,73 @@ const getDogById = async (id) => {
   }
 };
 
+// Get dogs by temperament
+const getDogsByTemperament = async (page, id) => {
+  const results = [];
+  try {
+    const dogs = await Dog.findAndCountAll({
+      attributes: ["id", "name", "image", "weight"],
+      include: {
+        model: Temperament,
+        where: {
+          id,
+        },
+      },
+      limit: 8,
+      offset: page * 8 - 8,
+    });
+
+    if (dogs.count === 0) {
+      return false;
+    }
+
+    if (dogs.rows.length > 0) {
+      for (let d of dogs.rows) {
+        const temperaments = await Temperament.findAll({
+          include: {
+            model: Dog,
+            where: {
+              id: d.id,
+            },
+          },
+        });
+
+        if (temperaments.length > 0) {
+          results.push({
+            id: d.id,
+            name: d.name,
+            image: d.image,
+            weight: d.weight,
+            temperaments: temperaments.map((t) => t.name),
+          });
+        } else {
+          results.push({
+            id: d.id,
+            name: d.name,
+            image: d.image,
+            weight: d.weight,
+            temperaments: [],
+          });
+        }
+      }
+
+      return {
+        totalResults: dogs.count,
+        totalPages: Math.ceil(dogs.count / 8),
+        page: parseInt(page),
+        data: results,
+      };
+    } else {
+      return { data: [] };
+    }
+  } catch (error) {
+    console.log(error.message);
+    throw new Error("Error trying to get all dogs by temperament");
+  }
+};
+
 module.exports = {
   getDogs,
   getDogById,
+  getDogsByTemperament,
 };
