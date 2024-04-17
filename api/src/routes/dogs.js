c7a8726d0b2d47f9be5f6415b6fcae839be2e95d
Ignacio Cunial <igsecu@hotmail.com>
2024-04-17 19:13:32 -0300

    Created GET /temperament/:id route

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -5,6 +5,8 @@ const dogController = require("../controllers/dogs");
 
 // Get dog by id
 router.get("/:id", dogController.getDogById);
+// Get dogs by temperament
+router.get("/temperament/:id", dogController.getDogsByTemperament);
 // Get all dogs
 router.get("/", dogController.getDogs);
 
