9cea3a75ee599a98a6d174715a0c38b639998c1c
Ignacio Cunial <igsecu@hotmail.com>
2024-04-18 19:06:49 -0300

    Created getTemperaments service

--- api/src/services/temperaments.js
+++ api/src/services/temperaments.js
@@ -20,6 +20,30 @@ const getTemperamentById = async (id) => {
   }
 };
 
+// Get temperaments
+const getTemperaments = async () => {
+  const results = [];
+  try {
+    const temperaments = await Temperament.findAll({
+      order: [["name", "ASC"]],
+    });
+
+    if (temperaments) {
+      temperaments.forEach((t) => {
+        results.push({
+          id: t.id,
+          name: t.name,
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    throw new Error("Error trying to get all temperaments");
+  }
+};
+
 module.exports = {
   getTemperamentById,
+  getTemperaments,
 };
