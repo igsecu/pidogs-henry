85809c72387c7deac648410a9ae175ce8eec9edb
Ignacio Cunial <igsecu@hotmail.com>
2024-04-19 20:55:23 -0300

    Created Carrousel component

---
+++ client/src/components/Carrousel.jsx
@@ -0,0 +1,65 @@
+const Carrousel = ({ comments }) => {
+  return (
+    <section className="p-3 mt-2">
+      <h3 className="border-bottom border-2 border-danger pb-2">
+        Last Comments
+      </h3>
+      <div
+        id="comments-carousel"
+        className="carousel slide carousel-dark mt-5 d-flex align-items-center border rounded shadow"
+        style={{ height: "300px" }}
+      >
+        <div className="carousel-inner text-center">
+          {comments.map((c, index) => (
+            <div
+              key={c.id}
+              className={index === 0 ? "carousel-item active" : "carousel-item"}
+            >
+              <img
+                src={c.dog.image}
+                className="rounded-circle "
+                alt="Dog"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  boxShadow: "1px 2px 5px black",
+                }}
+              />
+              <p className="fst-italic mt-3 mb-0">"{c.text}"</p>
+              <div className="text-center fw-bold">
+                <p className="mb-0">{c.dog.name}</p>
+                <small className="text-secondary">{c.from}</small>
+              </div>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#comments-carousel"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#comments-carousel"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Carrousel;
