5a09d013437d636294479f99ac07b9cedb8f9938
Ignacio Cunial <igsecu@hotmail.com>
2024-04-19 20:56:05 -0300

    Created MoreDogs component

---
+++ client/src/components/MoreDogs.jsx
@@ -0,0 +1,31 @@
+const MoreDogs = ({ dogs }) => {
+  return (
+    <div className="list-group rounded-0">
+      <p className="list-group-item border-0 border-bottom border-danger border-2 px-0 fw-bold">
+        More Views
+      </p>
+      {dogs.map((d) => (
+        <a
+          key={d.id}
+          href="/"
+          className="list-group-item list-group-item-action border-0 border-bottom"
+        >
+          <div className="d-flex">
+            <img
+              src={d.image}
+              alt=""
+              className="rounded-circle"
+              style={{ width: "30px", height: "30px" }}
+            />
+            <div className="ms-2 d-flex flex-lg-column w-100">
+              <p className="mb-0">{d.name}</p>
+              <span className="fw-bold ms-2 ms-lg-0">{d.views} views</span>
+            </div>
+          </div>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default MoreDogs;
