f37c44f36d924d5cd5ccf9cec66a08073201f10b
Ignacio Cunial <igsecu@hotmail.com>
2024-04-19 21:49:58 -0300

    Converted favorites Link to Dropdown menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import { FaDog } from "react-icons/fa";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ favorites }) => {
+  const location = useLocation();
+
   return (
     <nav className="navbar navbar-expand-md bg-dark navbar-dark py-3">
       <div className="container">
-        <a
-          href="/"
+        <NavLink
+          to="/"
           className="navbar-brand d-flex flex-row align-items-center fs-2 fw-bold"
         >
           <FaDog /> <span className="ms-2">Dogs</span>
-        </a>
+        </NavLink>
 
         <button
           className="navbar-toggler"
@@ -22,15 +25,42 @@ const Navbar = () => {
 
         <div className="collapse navbar-collapse" id="navmenu">
           <ul className="navbar-nav ms-auto">
-            <li className="navbar-item text-center text-start-md">
-              <a href="#learn" className="nav-link">
+            {location.pathname !== "/" && (
+              <li className="navbar-item text-center text-start-md">
+                <NavLink to="/" className="nav-link">
+                  Home
+                </NavLink>
+              </li>
+            )}
+            <li className="nav-item dropdown text-center text-start-md">
+              <span
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Favorites
-              </a>
+              </span>
+              <ul className="dropdown-menu rounded-0 bg-dark border-0 text-white">
+                {favorites ? (
+                  <p className=" mb-0 fw-bold text-center">
+                    You do not have favorites to show!
+                  </p>
+                ) : (
+                  <li>
+                    <a className="dropdown-item" href="/">
+                      Action
+                    </a>
+                  </li>
+                )}
+              </ul>
             </li>
+
             <li className="navbar-item bg-danger rounded text-center text-start-md custom-btn ms-2">
-              <a href="#questions" className="nav-link text-white ">
+              <Link to="/" className="nav-link text-white ">
                 Create Dog
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
