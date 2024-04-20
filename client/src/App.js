61ca3f10298bf919c3c50b94d5212adea387cf45
Ignacio Cunial <igsecu@hotmail.com>
2024-04-19 21:48:02 -0300

    Added dogs page and get favorites from local storage

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,26 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import { useState, useEffect } from "react";
+
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import DogsPage from "./pages/DogsPage";
 
 function App() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const itemsFromLocalStorage =
+      JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(itemsFromLocalStorage);
+  }, []);
+
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<MainLayout />}>
+      <Route path="/" element={<MainLayout favorites={favorites} />}>
         <Route index element={<HomePage />} />
+        <Route path="/dogs" element={<DogsPage />} />
       </Route>
     )
   );
