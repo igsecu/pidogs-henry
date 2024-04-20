8b211f516013636a827fa3adfa9899bf331eb988
Ignacio Cunial <igsecu@hotmail.com>
2024-04-19 21:50:16 -0300

    Passed favorites prop to Navbar

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const MainLayout = () => {
+const MainLayout = ({ favorites }) => {
   return (
     <>
-      <Navbar />
+      <Navbar favorites={favorites} />
       <Outlet />
       <Footer />
     </>
