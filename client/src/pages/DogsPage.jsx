6799a69770e279718fda9dc4d48567081d43167c
Ignacio Cunial <igsecu@hotmail.com>
2024-04-20 11:18:39 -0300

    Imported FilterNavbar component

--- client/src/pages/DogsPage.jsx
+++ client/src/pages/DogsPage.jsx
@@ -1,6 +1,9 @@
+import FilterNavbar from "../components/FilterNavbar";
+
 const DogsPage = () => {
   return (
     <section>
+      <FilterNavbar />
       <div className="container p-5" style={{ minHeight: "700px" }}></div>
     </section>
   );
