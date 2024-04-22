ee03c1c8485aad414f9a8efb3253b198c399a639
Ignacio Cunial <igsecu@hotmail.com>
2024-04-22 13:14:24 -0300

    Prop drilling to DogCard component

--- client/src/components/DogListings.jsx
+++ client/src/components/DogListings.jsx
@@ -1,10 +1,21 @@
 import DogCard from "./DogCard";
 
-const DogListings = ({ dogs }) => {
+const DogListings = ({
+  dogs,
+  addToFavorite,
+  removeFromFavorite,
+  favorites,
+}) => {
   return (
     <div className="d-flex flex-wrap justify-content-center">
       {dogs.map((d) => (
-        <DogCard key={d.id} dog={d} />
+        <DogCard
+          key={d.id}
+          dog={d}
+          addToFavorite={addToFavorite}
+          removeFromFavorite={removeFromFavorite}
+          favorites={favorites}
+        />
       ))}
     </div>
   );
